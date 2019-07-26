a5d24f296cc519ca1901e684045d535217bffc90
aesquivel94 <alejandraesqarias@gmail.com>
2019-07-26 14:21:15 -0500

    graficos...

--- Graficos_Paper.R
+++ Graficos_Paper.R
@@ -130,10 +130,6 @@ count_p <- p %>%
   group_by(zone, variety, ciclo, id) %>% 
   summarise(count = n())
 
-count_p %>% View()
-
-
-  
 # Hit score... matenme 
 
 mean_p <- p %>% 
@@ -146,33 +142,25 @@ mean_p <- p %>%
   
 # mean_p %>% dplyr::select(variety) %>% unique
 
-ggplot(mean_p %>% filter(variety == 'FNC3056'), aes(x = id, y = yield, colour = type)) + 
-  geom_line() +
-  geom_point() + 
-  geom_ribbon(aes(ymin=yield - y_sd, ymax= yield + y_sd , fill=type), alpha=0.2) + 
-  ylim(c(0, 10000)) +
-  scale_x_continuous(breaks = seq(from = 1, to = 12, by = 1)) +
-  facet_grid(ciclo~zone) + 
-  theme_bw()
-
-
-ggplot(mean_p %>% filter(variety == 'DK234'), aes(x = id, y = yield, colour = type)) + 
-  geom_line() +
-  geom_point() + 
-  geom_ribbon(aes(ymin=yield - y_sd, ymax= yield + y_sd , fill=type), alpha=0.2) + 
-  ylim(c(0, 10000)) +
-  scale_x_continuous(breaks = seq(from = 1, to = 12, by = 1)) +
-  facet_grid(ciclo~zone) + 
-  theme_bw()
-
-
-
-# =-=-= Promedio de promedios
-
-
 C <- as_labeller(c('1' = 'Cycle 1', '2' = 'Cycle 2'))
 Z <- as_labeller(c("CERETE" = 'Cereté (Córdoba)', "ESPINAL" = 'El Espinal (Tolima)',  "UNION" = 'La Unión (Valle del Cauca)'))
 
+
+
+ggplot(mean_p %>% filter(variety == 'FNC3056'), aes(x = id, y = yield, colour = type)) + 
+  geom_line() +
+  geom_point() + 
+  geom_ribbon(aes(ymin=yield - y_sd, ymax= yield + y_sd , fill=type), alpha=0.2)  + 
+  scale_fill_manual( NULL , values = c("gray30", "#008080"), labels = c('Sim', 'Obs')) +
+  scale_colour_manual(NULL , values = c("gray30", "#008080"), labels = c('Sim', 'Obs')) +
+  ylim(c(0, 10000)) +
+  scale_x_continuous(breaks = seq(from = 1, to = 12, by = 1)) +
+  facet_grid(ciclo~zone,  labeller = labeller(ciclo = C,  zone = Z)) + 
+  theme_bw() + 
+  labs(x = 'Planting date', y = 'Yield (Kg/ha)')
+
+
+# =-=-= Promedio de promedios
 mean_p %>% 
   group_by(zone, ciclo, id, type) %>% 
   summarise( y_sd= sd(yield, na.rm = TRUE), median_Y = median(yield), yield = mean(yield)) %>% 
@@ -189,16 +177,13 @@ mean_p %>%
   labs(x = 'Planting date', y = 'Yield (Kg/ha)')
 
 
-
-
+ 
 # library(verification)
 
 # ####### - ###### - ####### - ########
 
 
 # Mejor fecha de siembra...
-
-
 Datos_p <- p %>% 
   dplyr::select(-order_date, -pd, -month, -date, -cat) %>% 
   rename('Obs' = 'HWAM', 'Est' = 'HWAM.E') %>% 
@@ -208,14 +193,12 @@ Datos_p <- p %>%
   dplyr::select(-data) %>% 
   unnest()
  
-
-
+# =-=-=-=-=-= Listo... entonces de aquí en adelante 
 test <- Datos_p %>%
   nest(-year, -variety, -zone, -ciclo) %>% 
   mutate(Dif = purrr::map(.x = data, .f = function(.x){.x[1,-1] - .x[2,-1]})) %>% 
   dplyr::select(-data) %>% 
   unnest()
-
 
 
 test1 <- test %>%
@@ -227,14 +210,80 @@ test1 <- test %>%
   # filter(zone == 'UNION' , variety == 'DK234', ciclo == 1)
   
 
+# =-=-=-=-=-=-=-=
+test1 %>% 
+  ggplot(aes(x = id, y = n, fill = as.character(ciclo)))+
+  geom_bar(stat = 'identity', position = 'dodge', colour = 'black', alpha = 0.7) + 
+  facet_grid(zone ~ variety,  labeller = labeller(ciclo = C,  zone = Z)) + 
+  scale_x_continuous(breaks=0:4, labels=c('0', '5', "10", '15', '20+'))+
+  scale_fill_manual( ,values = c("gray30", "#008080"), labels = c('Sim', 'Obs')) +
+  scale_y_continuous(breaks = seq(from = 0, to = 9, by = 1)) + 
+  labs(x = 'Diference between planting dates obs - sim (days)', y = 'years', fill = 'Cycle') +
+  theme_bw() 
 
 
 
 
+test1 %>% 
+  ggplot(aes(x = id, y = n, fill = variety))+
+  geom_bar(stat = 'identity', position = 'dodge', colour = 'black', alpha = .7) + 
+  facet_grid(ciclo~zone,  labeller = labeller(ciclo = C,  zone = Z)) + 
+  scale_fill_viridis_d() + 
+  scale_x_continuous(breaks=0:4, labels=c('0', '5', "10", '15', '20+'))+
+  # scale_fill_manual( NULL , values = c("gray30", "#008080"), labels = c('Sim', 'Obs')) +
+  scale_y_continuous(breaks = seq(from = 0, to = 9, by = 1)) + 
+  labs(x = 'Diference between planting dates obs - sim (days)', y = 'years', fill = 'Variety') +
+  theme_bw() 
+
+
+
+#############################################################################################
+###  Acumulando 
+
+a <- Datos_p %>%
+  nest(-year, -variety, -zone, -ciclo) %>% 
+  mutate(Dif = purrr::map(.x = data, .f = function(.x){.x[1,-1] - .x[2,-1]})) %>% 
+  dplyr::select(-data) %>% 
+  unnest() %>% 
+  mutate(id = abs(id)) %>%
+  # dplyr::group_by(variety, zone, ciclo) %>% 
+  mutate(id = case_when( id > 3 ~ 4, id < -3 ~ -4, TRUE ~ as.numeric(id))) %>%
+  mutate(id = abs(id)) %>% 
+  count(zone, variety, zone, ciclo, id) %>% 
+  nest(-zone, -variety, -zone, -ciclo) %>% 
+  mutate(acum = purrr::map(.x = data, .f = function(.x){.x %>% mutate(n = cumsum(n)) })) %>% 
+  dplyr::select(-data) %>% 
+  unnest()
+
+
+a
 
 
 
 
+# =-=-=-=-=-=-=-=-=-=-=-=-=-= ejemplo de internet
+ggplot(mtcars, aes(x=`car name`, y=mpg_z, label=mpg_z)) + 
+  geom_point(stat='identity', fill="black", size=6)  +
+  geom_segment(aes(y = 0, 
+                   x = `car name`, 
+                   yend = mpg_z, 
+                   xend = `car name`), 
+               color = "black") +
+  geom_text(color="white", size=2) +
+  labs(title="Diverging Lollipop Chart", 
+       subtitle="Normalized mileage from 'mtcars': Lollipop") + 
+  ylim(-2.5, 2.5) +
+  coord_flip()
 
 
-
+# =-=-=-=
+test1 %>% 
+  ggplot(aes(x = id, y = n, , label=n,  colour = variety)) + 
+  geom_point(stat='identity', size=6)  +
+  geom_segment(aes(y = 0,  x = id,  yend = n,  xend =  id)) +
+  geom_text(color="white", size=2) + 
+  scale_x_continuous(breaks=0:4, labels=c('0', '5', "10", '15', '20+'))+
+  scale_y_continuous(breaks = seq(from = 0, to = 9, by = 1))   +
+  # coord_flip() +
+  facet_grid(ciclo ~ zone,  labeller = labeller(ciclo = C,  zone = Z)) +
+  theme_bw()
